e9852915ab77731dd7c99409df0ff3afabcf03e2
cristian erick laura velasquez <claurav@unjbg.edu.pe>
2026-07-08 09:47:18 -0500

    mejoramos e imprimimos la matriz

--- matrises1.cpp
+++ matrises1.cpp
@@ -7,11 +7,20 @@ int main(){
 	cin>>n;
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
-			cout<<"ingrese el dato"<<i+1<<":"<<j+1<<"=0";
+			cout<<"ingrese el dato"<<i+1<<":"<<j+1<<"=";
 			cin>>A[i][j];
 		}
 	}	
-	
+	cout<<"_____-LA MATRIZ_______"<<endl;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cout<<A[i][j];
+			if (j<n-1){
+				cout<<" ";
+			}
+}
+cout<<endl;
+}
 	
 	
 	return 0;
